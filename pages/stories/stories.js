3d58b091cb13584cb26c3bf9d589e9ad0c3dbe54
Valo-83 <2565681090@qq.com>
2026-06-04 20:13:35 +0800

    本地修改合并远端代码

--- pages/stories/stories.js
+++ pages/stories/stories.js
@@ -1,5 +1,8 @@
 // pages/stories/stories.js
-const { buildMyStoryAssets } = require('../words/storageUtils')
+const {
+  buildMyStoryAssets,
+  filterMyStoryAssets
+} = require('../words/storageUtils')
 const WORD_CATALOG = require('../words/wordCatalog')
 
 const STORY_STORAGE_KEY = 'LOCAL_STORED_AI_STORIES'
@@ -7,8 +10,25 @@ const STORY_AUDIO_STORAGE_KEY = 'LOCAL_STORY_AUDIO_FILES'
 
 Page({
   data: {
+    allStories: [],
     stories: [],
     hasStories: false,
+    hasFilteredStories: false,
+    searchKeyword: '',
+    categoryFilter: 'all',
+    audioFilter: 'all',
+    categoryOptions: [
+      { value: 'all', label: '全部' },
+      { value: 'animals', label: '动物' },
+      { value: 'fruits', label: '水果' },
+      { value: 'colors', label: '颜色' },
+      { value: 'numbers', label: '数字' }
+    ],
+    audioOptions: [
+      { value: 'all', label: '全部音频' },
+      { value: 'withAudio', label: '已缓存音频' },
+      { value: 'withoutAudio', label: '未缓存音频' }
+    ],
     playingStoryId: ''
   },
 
@@ -31,14 +51,52 @@ Page({
   loadMyStories() {
     this.readLocalStorage(STORY_STORAGE_KEY, (stories) => {
       this.readLocalStorage(STORY_AUDIO_STORAGE_KEY, (audioCache) => {
-        const storyAssets = buildMyStoryAssets(stories, audioCache, WORD_CATALOG)
+        const allStories = buildMyStoryAssets(stories, audioCache, WORD_CATALOG)
+        const storyAssets = this.filterStories(allStories)
         this.setData({
+          allStories,
           stories: storyAssets,
-          hasStories: storyAssets.length > 0,
+          hasStories: allStories.length > 0,
+          hasFilteredStories: storyAssets.length > 0,
           playingStoryId: ''
         })
       })
     })
+  },
+
+  filterStories(stories) {
+    return filterMyStoryAssets(stories, {
+      keyword: this.data.searchKeyword,
+      category: this.data.categoryFilter,
+      audioStatus: this.data.audioFilter
+    })
+  },
+
+  applyFilters() {
+    const stories = this.filterStories(this.data.allStories)
+    this.setData({
+      stories,
+      hasFilteredStories: stories.length > 0,
+      playingStoryId: ''
+    })
+  },
+
+  onSearchInput(e) {
+    this.setData({
+      searchKeyword: e.detail.value || ''
+    }, () => this.applyFilters())
+  },
+
+  selectCategory(e) {
+    this.setData({
+      categoryFilter: e.currentTarget.dataset.value || 'all'
+    }, () => this.applyFilters())
+  },
+
+  selectAudio(e) {
+    this.setData({
+      audioFilter: e.currentTarget.dataset.value || 'all'
+    }, () => this.applyFilters())
   },
 
   playAudio(e) {
